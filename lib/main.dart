fc4c4170c3617de33fc93025346d3ea1459f46ac
Yehia F <yehia.fahmy@uwaterloo.ca>
2021-02-19 12:13:44 -0500

    another include statement

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'dart:io';
 import 'package:image_picker/image_picker.dart';
 import 'package:tflite/tflite.dart';
+//import 'package:image/image.dart';
 
 void main() {
   runApp(MaterialApp(
